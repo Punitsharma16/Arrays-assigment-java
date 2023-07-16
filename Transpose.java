a48665dedf5aded6c95b3c8849ccb580df6d1cb7
Punitsharma16 <punitsharma9034908543@gmail.com>
2023-07-16 17:27:15 +0530

    java

---
+++ Transpose.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Transpose {
+        //    function for transpose
+        static void transpose(int[][] arr , int n){
+            int[][] transposeArr = new int[n][n];
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    transposeArr[i][j] = arr[j][i];
+                }
+            }
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    System.out.print(transposeArr[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the length of the array");
+            int n = sc.nextInt();
+            System.out.println("Enter the matrix array");
+            int[][] arr = new int[n][n];
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            transpose(arr, n);
+        }
+    }
+
