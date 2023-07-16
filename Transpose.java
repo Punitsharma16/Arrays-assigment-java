a607e353f89915884a49729d288b5da4771adf66
Punitsharma16 <punitsharma9034908543@gmail.com>
2023-07-16 17:27:50 +0530

    java

--- Transpose.java
+++ Transpose.java
@@ -1,6 +1,5 @@
 import java.util.*;
 public class Transpose {
-        //    function for transpose
         static void transpose(int[][] arr , int n){
             int[][] transposeArr = new int[n][n];
             for(int i=0; i<n; i++){
